9e6e571f0d8b7deae5b7d111eacd7f4db98fa38f
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-15 17:37:44 +0200

    Created paintings controller, index, new and create actions and index view

---
+++ app/controllers/paintings_controller.rb
@@ -0,0 +1,24 @@
+class PaintingsController < ApplicationController
+  def index
+    @paintings = Painting.all
+  end
+
+  def new
+    @painting = Painting.new
+  end
+
+  def create
+    @painting = Painting.new(painting_params)
+    if @painting.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def painting_params
+    params.require(:paintings).permit(:name, :description, :year, :style, :available, :price, :photo)
+  end
+end
