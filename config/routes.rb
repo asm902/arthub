58d9eb0048d732076d8f1194b3b26ab84f91ee2a
Adam McCallister <adam@adammccallister.com>
2019-07-15 15:34:09 +0200

    completed the routes for our models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :bookings, only: [:new, :create, :update, :edit]
   root to: 'pages#home'
+
+  resources :paintings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
