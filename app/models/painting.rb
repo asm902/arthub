fb326784ea92f89ceacf71347206f610385f3504
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-16 17:46:55 +0200

    added dates to booking form, formatted show page further

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -2,11 +2,4 @@ class Painting < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
   validates :style, inclusion: { in: ["Nature", "Abstract", "Nude", "Impressionism", "Cubism"] }
-
-  # def unavailable_dates
-  #   bookings.pluck(:start_date, :end_date).map do |range|
-  #     { from: range[0], to: range[1] }
-  #   end
-  # end
-
 end
