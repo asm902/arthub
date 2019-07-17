011f19dc1131b1a5935733b08e7a3b7175236914
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-17 15:45:38 +0200

    booking completed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,17 +3,21 @@
   def new
     @painting = Painting.find(params[:painting_id])
     @booking = Booking.new
+    @start_date = params[:end_date]
+    @end_date = params[:end_date]
     @duration = (params[:end_date].to_date - params[:start_date].to_date).to_i
     @total_price = @duration * @painting.price
   end
 
   def create
+    @duration = (booking_params[:end_date].to_date - booking_params[:start_date].to_date).to_i
     @painting = Painting.find(params[:painting_id])
+    total_price = @duration * @painting.price
     @booking = Booking.new(booking_params)
-    @booking.painting = @painting
     @booking.user = current_user
-    @booking.total_price = @total_price
-    if @booking.save
+    @booking.total_price = total_price
+    @booking.painting = @painting
+    if @booking.save!
       redirect_to painting_path(@painting)
     else
       render 'paintings/show'
