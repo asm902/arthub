a2fa52f89e631265d57ec3cf7e4936db8818ec42
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-17 11:48:08 +0200

    add-date functionality is working, seed amended to add user photos, total price added to Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,18 @@
-class BookingsController < ApplicationController
+  class BookingsController < ApplicationController
+
+  def new
+    @painting = Painting.find(params[:painting_id])
+    @booking = Booking.new
+    @duration = (params[:end_date].to_date - params[:start_date].to_date).to_i
+    @total_price = @duration * @painting.price
+  end
+
   def create
     @painting = Painting.find(params[:painting_id])
     @booking = Booking.new(booking_params)
     @booking.painting = @painting
     @booking.user = current_user
+    @booking.total_price = @total_price
     if @booking.save
       redirect_to painting_path(@painting)
     else
